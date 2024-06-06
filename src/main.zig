e9a4a9ff1d940856b80d5573cf30c398bd159e30
Felipe Coury <felipe.coury@gmail.com>
2024-06-06 01:04:32 -0300

    feat: initial set of integrated tests

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,15 @@
 const std = @import("std");
 const runner = @import("runner.zig");
+const tests = @import("tests.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
-    defer _ = gpa.deinit();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
+    // defer _ = gpa.deinit();
+    //
+    // const alloc = gpa.allocator();
+    //
+    // try runner.runTest(alloc, "SingleStepTests-z80/v1/0a.json");
+    // std.debug.print("SingleStepTests-z80/v1/0a.json passed\n", .{});
 
-    const alloc = gpa.allocator();
-
-    try runner.runTest(alloc, "SingleStepTests-z80/v1/0a.json");
+    std.debug.print("Tests: {any}\n", .{tests.Tests.len});
 }
